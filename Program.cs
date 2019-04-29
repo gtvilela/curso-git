149ac61bf3a992021ddd7b91fd74ed0239618a2d
Gabriela Vilela <gabrielathalitasv@gmail.com>
2019-04-29 13:02:03 -0300

    Acrescimo: Desejando um bom dia

--- Program.cs
+++ Program.cs
@@ -7,13 +7,14 @@ namespace teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como você está?");
 
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("oi");
             }
+
+            Console.WriteLine("Tenha um bom dia");
 
             Console.ReadKey();
         }
